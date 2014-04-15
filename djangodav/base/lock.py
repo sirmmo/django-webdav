4a99df6b81ec4497710b58f55c8c70f5543f44ce
meteozond <alex@erix.ru>
2014-04-15 17:23:50 +0400

    - DavLock

--- djangodav/base/lock.py
+++ djangodav/base/lock.py
@@ -1,39 +1,41 @@
-from django.utils import synch
+# Portions (c) 2014, Alexander Klimenko <alex@erix.ru>
+# All rights reserved.
+#
+# Copyright (c) 2011, SmartFile <btimby@smartfile.com>
+# All rights reserved.
+#
+# This file is part of DjangoDav.
+#
+# DjangoDav is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# DjangoDav is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with DjangoDav.  If not, see <http://www.gnu.org/licenses/>.
 
 
-class DavLock(object):
-    def __init__(self, server):
-        self.server = server
-        self.lock = synch.RWLock()
+class BaseLock(object):
+    def __init__(self, resource):
+        self.resource = resource
 
-    def get(self, res):
+    def get(self):
         """Gets all active locks for the requested resource. Returns a list of locks."""
-        self.lock.reader_enters()
-        try:
-            pass
-        finally:
-            self.lock.reader_leaves()
+        raise NotImplementedError()
 
-    def acquire(self, res, type, scope, depth, owner, timeout):
+    def acquire(self, type, scope, depth, owner, timeout):
         """Creates a new lock for the given resource."""
-        self.lock.writer_enters()
-        try:
-            pass
-        finally:
-            self.lock.writer_leaves()
+        raise NotImplementedError()
 
-    def release(self, lock):
+    def release(self):
         """Releases the lock referenced by the given lock id."""
-        self.lock.writer_enters()
-        try:
-            pass
-        finally:
-            self.lock.writer_leaves()
+        raise NotImplementedError()
 
-    def del_locks(self, res):
+    def del_locks(self):
         """Releases all locks for the given resource."""
-        self.lock.writer_enters()
-        try:
-            pass
-        finally:
-            self.lock.writer_leaves()
+        raise NotImplementedError()
