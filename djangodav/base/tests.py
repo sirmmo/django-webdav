93ccf5424cef967bff0a12c5305df6ce00cb7942
meteozond <alex@erix.ru>
2014-04-17 12:56:31 +0400

    * splitting base and fs tests

--- djangodav/base/tests.py
+++ djangodav/base/tests.py
@@ -20,7 +20,6 @@
 # along with DjangoDav.  If not, see <http://www.gnu.org/licenses/>.
 from django.test import TestCase
 from djangodav.base.resource import BaseDavResource
-from djangodav.fs.resource import BaseFSDavResource
 from mock import patch, Mock
 
 
@@ -49,47 +48,3 @@ class TestBaseDavResource(TestCase):
 
     def test_displayname(self):
         self.assertEqual(self.resource.displayname, 'name')
-
-
-class TestFSDavResource(TestCase):
-    class FSDavResource(BaseFSDavResource):
-        base_url = 'http://testserver/base/'
-        root = '/some/folder/'
-
-    def setUp(self):
-        self.resource = self.FSDavResource("/path/to/name")
-
-    @patch('djangodav.fs.resource.os.path.isdir')
-    def test_is_collection(self, isdir):
-        isdir.return_value = True
-        self.assertTrue(self.resource.is_collection())
-        isdir.assert_called_with('/some/folder/path/to/name')
-
-    @patch('djangodav.fs.resource.os.path.isfile')
-    def test_isfile(self, isfile):
-        isfile.return_value = True
-        self.assertTrue(self.resource.is_object())
-        isfile.assert_called_with('/some/folder/path/to/name')
-
-    @patch('djangodav.fs.resource.os.path.exists')
-    def test_isfile(self, exists):
-        exists.return_value = True
-        self.assertTrue(self.resource.exists())
-        exists.assert_called_with('/some/folder/path/to/name')
-
-    @patch('djangodav.fs.resource.os.path.getsize')
-    def test_get_size(self, getsize):
-        getsize.return_value = 42
-        self.assertEquals(self.resource.getcontentlength, 42)
-        getsize.assert_called_with('/some/folder/path/to/name')
-
-    def test_get_abs_path(self):
-        self.assertEquals(self.resource.get_abs_path(), '/some/folder/path/to/name')
-
-    @patch('djangodav.fs.resource.os.listdir')
-    def test_get_children(self, listdir):
-        listdir.return_value=['child1', 'child2']
-        children = list(self.resource.get_children())
-        self.assertEqual(children[0].path, ['path', 'to', 'name', 'child1'])
-        self.assertEqual(children[1].path, ['path', 'to', 'name', 'child2'])
-        listdir.assert_called_with('/some/folder/path/to/name')
