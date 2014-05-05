c98fc0c12843b518de5113dfb6825eedbb5fdc86
meteozond <alex@erix.ru>
2014-05-05 15:28:20 +0400

    *  version

--- djangodav/__init__.py
+++ djangodav/__init__.py
@@ -22,5 +22,5 @@
 from functools import partial
 from django import get_version as base_get_version
 
-VERSION = (0, 0, 1, 'alpha', 0)
+VERSION = (0, 0, 1, 'beta', 0)
 get_version = partial(base_get_version, VERSION)
