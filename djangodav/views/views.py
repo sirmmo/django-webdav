81cbcb50025e201f2b4370aceb687175048fb606
meteozond <alex@erix.ru>
2014-05-21 15:58:11 +0400

    * Check allowed methods over DavView._allowed_methods instead of DavView.http_method_names This fixes #15

--- djangodav/views/views.py
+++ djangodav/views/views.py
@@ -49,7 +49,7 @@ class DavView(View):
                 namespaces=WEBDAV_NSMAP
             )
 
-        if request.method.lower() in self.http_method_names:
+        if request.method.upper() in self._allowed_methods():
             handler = getattr(self, request.method.lower(), self.http_method_not_allowed)
         else:
             handler = self.http_method_not_allowed
