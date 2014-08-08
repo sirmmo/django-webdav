3d4fd6595dd84baad4582b8a2a72271041fc07f7
meteozond <alex@erix.ru>
2014-08-08 20:45:32 +0400

    - ghost folder in root

--- djangodav/__init__.py
+++ djangodav/__init__.py
@@ -22,5 +22,5 @@
 from functools import partial
 from django import get_version as base_get_version
 
-VERSION = (0, 0, 1, 'beta', 4)
+VERSION = (0, 0, 1, 'beta', 5)
 get_version = partial(base_get_version, VERSION)
