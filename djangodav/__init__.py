1c1178dd7ec07f863cbcd97c319cb223e0dec867
meteozond <alex@erix.ru>
2014-11-26 14:55:42 +0400

    * bump version

--- djangodav/__init__.py
+++ djangodav/__init__.py
@@ -22,5 +22,5 @@
 from functools import partial
 from django import get_version as base_get_version
 
-VERSION = (0, 0, 1, 'beta', 9)
+VERSION = (0, 0, 1, 'beta', 10)
 get_version = partial(base_get_version, VERSION)
