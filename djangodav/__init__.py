6060731bc6a2563e571a6ee2fc224befca756735
meteozond <alex@erix.ru>
2017-02-27 14:08:52 +0300

    * bump version 0.0.1b22

--- djangodav/__init__.py
+++ djangodav/__init__.py
@@ -19,5 +19,5 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with DjangoDav.  If not, see <http://www.gnu.org/licenses/>.
 
-VERSION = (0, 0, 1, 'beta', 20)
-__version__ = "0.0.1b21"
+VERSION = (0, 0, 1, 'beta', 22)
+__version__ = "0.0.1b22"
